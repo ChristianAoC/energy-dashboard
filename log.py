7f4d073d180b48a00c6a11fa6749853c0b811507
Luke Needle <l.needle@lancaster.ac.uk>
2025-08-08 13:51:32 +0100

    Added documentation to log levels

--- log.py
+++ log.py
@@ -3,9 +3,37 @@ import datetime as dt
 from database import db
 import models
 
+# Levels were loosly based on https://stackoverflow.com/a/2031209 (and other anwers for that question),
+# along with past experience.
+
+# Any general useful information, e.g:
+# - starting/finishing an operation
+# - processing times
+# - bypassing authentication (in a normal case)
+# - skipping something (a meter/building in a health check / summary)
+# Useful for debugging and must not require direct intervention, it should be safe to ignore/disgard
 info = "info"
+
+# An error that doesn't stop the operation from continuing, e.g:
+# - an error that occurs while reading an individual record from a json/csv/xlsx file
+# - an error while reading a cache, but we can just recalculate the information
+# - an error saving a cache but we can still return the information
+# May require direct intervention from an administrator
 warning = "warning"
+
+# An error that stops the operation from continuing *but not the service as a whole*, e.g:
+# - an internal api call fails
+# - unable to locate offline data
+# - an error occurs generating cache that cannot be recovered from
+# Should require direct intervention from an administrator
+# Note: These logs should contain both a message and extra info
 error = "error"
+
+# An error that stops the service from running/starting, e.g:
+# - a required file is missing
+# - can't access a 
+# Requires direct, immediate intervention from an adminitrator
+# Note: These logs must contain both a message and extra info
 critical = "critical"
 
 index = {
