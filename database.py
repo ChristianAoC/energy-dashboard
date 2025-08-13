e6038d65cb44a02cc3d6599b32c19f6f2a29ceac
Luke Needle <l.needle@lancaster.ac.uk>
2025-08-13 18:21:14 +0100

    Added note to database.py

--- database.py
+++ database.py
@@ -9,6 +9,7 @@ import sys
 
 from constants import metadata_file, building_mappings, meter_mappings, offline_data_files, offline_meta_file
 
+# TODO: Test with default settings again to see if we actually need to change these
 db = SQLAlchemy(engine_options={
     "pool_size": 20,
     "max_overflow": 20
