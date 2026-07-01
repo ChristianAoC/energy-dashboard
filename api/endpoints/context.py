252d0d4156b516cf7fccbdd3ba5c47cd230468ef
Luke Needle <l.needle@lancaster.ac.uk>
2026-07-01 14:20:52 +0100

    Restrict author forging for context creation to admins

--- api/endpoints/context.py
+++ api/endpoints/context.py
@@ -3,6 +3,10 @@ from flask import Blueprint, make_response, request, jsonify, g
 import api.context as context
 import api.endpoints.data as data_api_bp
 import api.users as users
+import log
+
+from datetime import datetime
+
 
 context_api_bp = Blueprint('context_api_bp', __name__, static_url_path='')
 
@@ -18,13 +22,34 @@ def add_context():
     if context_elem is None:
         return make_response("Invalid context", 400)
     
+    cookies = request.cookies
+    email = cookies.get("Email", None)
+    session_id = cookies.get("SessionID", None)
+    
     # for global mute run user level check just to be sure
     if context_elem["type"] == "Global-mute":
-        cookies = request.cookies
-        email = cookies.get("Email", None)
-        session_id = cookies.get("SessionID", None)
         if users.get_user_level(email, session_id) < g.settings["user_level_edit_comments"]:
             return make_response("Unauthorised", 403)
+    
+    # Check that the author is the current user, only admins can forge authorship
+    if context_elem["author"] != email:
+        if users.get_user_level(email, session_id) < g.settings["user_level_admin"]:
+            return make_response("Unauthorised", 403)
+        start_time = context_elem.get("start")
+        if start_time is not None:
+            start_time = datetime.strptime(start_time,"%Y-%m-%d %H:%M")
+        start_timestamp = start_time
+
+        end_time = context_elem.get("end")
+        if end_time is not None:
+            end_time = datetime.strptime(end_time,"%Y-%m-%d %H:%M")
+        end_timestamp = end_time
+        
+        log_msg = f"WARNING: User {email} forged authorship of {context_elem['author']} for the following context item:"
+        log_extra_info = f"target_type={context_elem['target_type'].lower()};target_id={context_elem['target_id']};context_type={context_elem['type']};start_timestamp={start_timestamp};end_timestamp={end_timestamp};deleted={context_elem.get('deleted', False)}"
+        log.write(msg=log_msg, level=log.warning, extra_info=log_extra_info)
+        print(log_msg)
+        print(log_extra_info)
     
     return context.add_context(context_elem)
 
